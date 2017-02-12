04b0eaed83b96b968fa5d4f803dda33240cd46cb
Swarup <swarupduttaroy95ms@gmail.com>
2017-02-12 20:52:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ element.innerHtml= 'New Value';
 //move the image
 var img = document.getElementById('madi');
 img.onClick = function() {
+    var interval = setInterval(moveRight, 100);
   img.style.marginleft = '100px';  
 };
 

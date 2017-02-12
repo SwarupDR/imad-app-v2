8ca5e75fe1318c114086fd45a952b4195b3a9266
Swarup <swarupduttaroy95ms@gmail.com>
2017-02-12 21:25:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,13 @@
 console.log('Loaded!');
 //changing text of the main text div
-var element = document.getElementById('main-text');
-element.innerHtml= 'New Value';
+var element = document.getElementById('main-text'
+);
+element.innerHTML= 'New Value';
 //move the image
 var img = document.getElementById('madi');
-var marginLeft = 0;
- fuction moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
 img.onClick = function() {
     var interval = setInterval(moveRight, 50);
-  img.style.marginleft = '100px';  
+  img.style.marginLeft = '100px';  
 };
 
 
